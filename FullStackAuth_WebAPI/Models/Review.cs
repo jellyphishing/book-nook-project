c6014189f31b677fecddbdc31627dc85f969bcdb
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-17 11:00:59 -0700

    added models for Fav and Review class

--- FullStackAuth_WebAPI/Models/Review.cs
+++ FullStackAuth_WebAPI/Models/Review.cs
@@ -1,6 +1,26 @@
-﻿namespace FullStackAuth_WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+
+
+
+namespace FullStackAuth_WebAPI.Models
 {
     public class Review
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ThumbnailUrl { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
     }
 }
