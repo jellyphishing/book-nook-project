49d90c168a85f9d9f8a9cbc9c1adc36621981675
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-17 09:09:44 -0700

    addded all controllers and DTO classes

---
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.DataTransferObjects;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+       // [HttpPost, Authorize]
+
+       // public IActionResult Post([FromBody] Review data)
+       //{}
+    
+    }
+}
